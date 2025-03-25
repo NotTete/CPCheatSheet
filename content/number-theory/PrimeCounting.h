fbc846c58cd0de4b184c2952a0d8fc48b195b340
N41h-Dr3s <n41hdr3s@gmail.com>
2025-03-25 10:47:04 +0100

    Arreglar Mates xD

--- content/number-theory/PrimeCounting.h
+++ content/number-theory/PrimeCounting.h
@@ -3,23 +3,25 @@
  * Date: 2025-03-16
  * Description: Given an integer it gives you $\pi$(n). 
  * Time: O(n^{3/4})
- * Status: Not-tested
+ * Status: teted only with completly multipicative functions
  */
 #pragma once
-int count_primes(int n) {
-	int sq=sqrt(n),a=0;
-	set<int> V;
-	for (int k = 1; k * k <= n; ++k)	V.insert(n / k), V.insert(k);
-	vector<int> v(V.begin(), V.end()), dp = v;
-	auto geti = [&](int x) {return (x>sq? (int)(v.size() - n/x):x-1);};
-	for (int p = 2; p * p <= n; p++) {
-		if (dp[geti(p)] != dp[geti(p - 1)]) {
-			a++;
-			for (int i = (int)v.size() - 1; i >= 0; i--) {
-				if (v[i] < p * p) break;
-				dp[i] -= dp[geti(v[i] / p)] - a;
-			}
-		}
-	}
-	return dp[geti(n)] - 1;
+int count_primes(int n) { 
+    auto f = [&](int n) {return 1;}; // (f(ab) = f(a)f(b))
+    auto pref = [&](int n) {return n;}; // should return sum_{i=1..n} f(i)
+    vector<int> v; v.reserve((int)sqrt(n) * 2 + 20);
+    int sq, k = 1;
+    for (; k * k <= n; k++) v.push_back(k);
+    sq = --k;
+    if (k * k == n) k--;
+    for (; k >= 1; k--) v.push_back(n / k);
+    vector<int> s(v.size());
+    for (int i = 0; i < s.size(); i++) s[i] = pref(v[i]) - 1;
+    auto geti = [&](int x) {return (x<=sq? (int)x-1:(int)(v.size() - (n / x)));};
+    for (int p = 2; p * p <= n; p++)
+        if (s[p - 1] != s[p - 2])
+            for (int i = (int)v.size() - 1; p*p <= v[i] && i >= 0; --i)
+                s[i] -= (s[geti(v[i] / p)] - s[p-2]) * f(p);
+
+    return s.back();
 }
